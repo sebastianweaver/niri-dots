b5385e7fb241a25713db8f03748a18819dc0e6fa
cbash <sebastian33weaver@gmail.com>
2025-08-12 17:30:15 -0500

    update all again

--- scripts/swaylock.sh
+++ scripts/swaylock.sh
@@ -12,16 +12,16 @@ swaylock \
   --effect-blur 4x2 \
   --datestr "%A, %b %d" \
   --timestr "%k:%M" \
-  --key-hl-color 838379f2 \
-  --ring-color 838379f2 \
-  --text-color c6c2c2e6 \
-  --inside-clear-color 1b0c0cf2 \
-  --ring-clear-color 838379f2 \
-  --text-clear-color c6c2c2e6 \
-  --inside-ver-color 1b0c0cf2 \
-  --ring-ver-color 838379f2 \
-  --text-ver-color c6c2c2e6 \
-  --bs-hl-color 687272ff \
-  --inside-wrong-color 5F8788ff \
-  --ring-wrong-color 5F8788ff \
-  --text-wrong-color c6c2c2ff
+  --key-hl-color 6C6A5Ff2 \
+  --ring-color 6C6A5Ff2 \
+  --text-color c3c5c8e6 \
+  --inside-clear-color 101725f2 \
+  --ring-clear-color 6C6A5Ff2 \
+  --text-clear-color c3c5c8e6 \
+  --inside-ver-color 101725f2 \
+  --ring-ver-color 6C6A5Ff2 \
+  --text-ver-color c3c5c8e6 \
+  --bs-hl-color 4C4D4Fff \
+  --inside-wrong-color 6F8F70ff \
+  --ring-wrong-color 6F8F70ff \
+  --text-wrong-color c3c5c8ff
