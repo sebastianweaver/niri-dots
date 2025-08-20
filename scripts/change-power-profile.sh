b3eea216d113c46d814b83b9a08e2b3a61c1a2bd
cbash <sebastian33weaver@gmail.com>
2025-08-20 11:41:55 -0500

    minor adjustments

--- scripts/change-power-profile.sh
+++ scripts/change-power-profile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-options="powersave\nbalanced\nperformance"
+options="power-saver\nbalanced\nperformance"
 choice=$(echo -e "$options" | fuzzel --dmenu --lines 3 -w 20 --config $HOME/.config/fuzzel/power-profile.ini)
 if [ ! -z "$choice" ]; then
-  sudo pkill tuned
+  sudo pkill powerprofilesctl
   echo $choice >$HOME/.local/state/power-profile
-  sudo tuned --profile $choice &
+  sudo powerprofilesctl set $choice &
   disown
 fi
--- scripts/swayidle.sh
+++ scripts/swayidle.sh
@@ -10,28 +10,28 @@ case $idle_time in
   swayidle -w \
     timeout 300 $lock \
     timeout 420 'niri msg action power-off-monitors' \
-    timeout 600 'systemctl suspend' \
+    timeout 600 'systemctl suspend-then-hibernate' \
     before-sleep $lock
   ;;
 "10 minutes")
   swayidle -w \
     timeout 600 $lock \
     timeout 720 'niri msg action power-off-monitors' \
-    timeout 1200 'systemctl suspend' \
+    timeout 1200 'systemctl suspend-then-hibernate' \
     before-sleep $lock
   ;;
 "20 minutes")
   swayidle -w \
     timeout 1200 $lock \
     timeout 1500 'niri msg action power-off-monitors' \
-    timeout 2400 'systemctl suspend' \
+    timeout 2400 'systemctl suspend-then-hibernate' \
     before-sleep $lock
   ;;
 "30 minutes")
   swayidle -w \
     timeout 1800 $lock \
     timeout 2100 'niri msg action power-off-monitors' \
-    timeout 3600 'systemctl suspend' \
+    timeout 3600 'systemctl suspend-then-hibernate' \
     before-sleep $lock
   ;;
 "infinity") ;;
