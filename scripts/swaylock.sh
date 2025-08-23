1b81950887df55af2660e37aef76ccaa50a97d6b
cbash <sebastian33weaver@gmail.com>
2025-08-22 22:42:06 -0500

    cursor and theme change

--- scripts/swaylock.sh
+++ scripts/swaylock.sh
@@ -12,16 +12,16 @@ swaylock \
   --effect-blur 4x2 \
   --datestr "%A, %b %d" \
   --timestr "%k:%M" \
-  --key-hl-color 6C6A5Ff2 \
-  --ring-color 6C6A5Ff2 \
-  --text-color c3c5c8e6 \
-  --inside-clear-color 101725f2 \
-  --ring-clear-color 6C6A5Ff2 \
-  --text-clear-color c3c5c8e6 \
-  --inside-ver-color 101725f2 \
-  --ring-ver-color 6C6A5Ff2 \
-  --text-ver-color c3c5c8e6 \
-  --bs-hl-color 4C4D4Fff \
-  --inside-wrong-color 6F8F70ff \
-  --ring-wrong-color 6F8F70ff \
-  --text-wrong-color c3c5c8ff
+  --key-hl-color 33364Af2 \
+  --ring-color 33364Af2 \
+  --text-color c3c1c5e6 \
+  --inside-clear-color 100a19f2 \
+  --ring-clear-color 33364Af2 \
+  --text-clear-color c3c1c5e6 \
+  --inside-ver-color 100a19f2 \
+  --ring-ver-color 33364Af2 \
+  --text-ver-color c3c1c5e6 \
+  --bs-hl-color 16364Cff \
+  --inside-wrong-color 244E64ff \
+  --ring-wrong-color 244E64ff \
+  --text-wrong-color c3c1c5ff
