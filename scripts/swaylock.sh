97d5bbdb6d4b866909e38ff7898f0da267fbf7a5
cbash <sebastian33weaver@gmail.com>
2025-09-09 21:42:42 -0500

    update templates for pywal

--- scripts/swaylock.sh
+++ scripts/swaylock.sh
@@ -12,16 +12,16 @@ swaylock \
   --effect-blur 4x2 \
   --datestr "%A, %b %d" \
   --timestr "%k:%M" \
-  --key-hl-color 6D685Bf2 \
-  --ring-color 6D685Bf2 \
-  --text-color c3c3c3e6 \
-  --inside-clear-color 111111f2 \
-  --ring-clear-color 6D685Bf2 \
-  --text-clear-color c3c3c3e6 \
-  --inside-ver-color 111111f2 \
-  --ring-ver-color 6D685Bf2 \
-  --text-ver-color c3c3c3e6 \
-  --bs-hl-color 505049ff \
-  --inside-wrong-color 7F816Fff \
-  --ring-wrong-color 7F816Fff \
-  --text-wrong-color c3c3c3ff
+  --key-hl-color 4C4F4Ff2 \
+  --ring-color 4C4F4Ff2 \
+  --text-color c3c5c6e6 \
+  --inside-clear-color 121a1bf2 \
+  --ring-clear-color 4C4F4Ff2 \
+  --text-clear-color c3c5c6e6 \
+  --inside-ver-color 121a1bf2 \
+  --ring-ver-color 4C4F4Ff2 \
+  --text-ver-color c3c5c6e6 \
+  --bs-hl-color 3D6446ff \
+  --inside-wrong-color 8C6D66ff \
+  --ring-wrong-color 8C6D66ff \
+  --text-wrong-color c3c5c6ff
