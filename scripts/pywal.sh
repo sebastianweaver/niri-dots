2840d8a02d6e24ccebb690b5cdaaa0edc180713a
cbash <sebastian33weaver@gmail.com>
2025-09-12 12:17:28 -0500

    correcting pywal generation scripts

--- scripts/pywal.sh
+++ scripts/pywal.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 wp=$HOME/Pictures/Wallpapers/
 
-niri msg action do-screen-transition --delay-ms 500 &&
+niri msg action do-screen-transition &&
   wal -i "$wp" -o pywal-discord
 $XDG_CONFIG_HOME/mako/update-theme.sh
 $XDG_CONFIG_HOME/scripts/build-gradience.py
@@ -10,6 +10,4 @@ gradience-cli apply -n pywal
 spicetify apply
 cp "${HOME}/.cache/wal/niri-config.kdl" "${HOME}/.config/niri/config.kdl" &&
   . ~/.config/niri/scripts/niri-pywal-template.sh
-systemctl --user reload waybar.service &&
-  sleep 1 &&
-  niri msg action do-screen-transition --delay-ms 500
+systemctl --user reload waybar.service && niri msg action do-screen-transition -d 800 && sleep 1
--- scripts/swaylock.sh
+++ scripts/swaylock.sh
@@ -12,16 +12,16 @@ swaylock \
   --effect-blur 4x2 \
   --datestr "%A, %b %d" \
   --timestr "%k:%M" \
-  --key-hl-color F09104f2 \
-  --ring-color F09104f2 \
-  --text-color c1c0c0e6 \
-  --inside-clear-color 090404f2 \
-  --ring-clear-color F09104f2 \
-  --text-clear-color c1c0c0e6 \
-  --inside-ver-color 090404f2 \
-  --ring-ver-color F09104f2 \
-  --text-ver-color c1c0c0e6 \
-  --bs-hl-color B6851Eff \
-  --inside-wrong-color FAAC32ff \
-  --ring-wrong-color FAAC32ff \
-  --text-wrong-color c1c0c0ff
+  --key-hl-color 70A49Bf2 \
+  --ring-color 70A49Bf2 \
+  --text-color cdc3c3e6 \
+  --inside-clear-color 3a1212f2 \
+  --ring-clear-color 70A49Bf2 \
+  --text-clear-color cdc3c3e6 \
+  --inside-ver-color 3a1212f2 \
+  --ring-ver-color 70A49Bf2 \
+  --text-ver-color cdc3c3e6 \
+  --bs-hl-color C46996ff \
+  --inside-wrong-color CF9BA6ff \
+  --ring-wrong-color CF9BA6ff \
+  --text-wrong-color cdc3c3ff
